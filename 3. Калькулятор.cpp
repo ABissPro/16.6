519b625043639a5b56fe4fcd3775eb1f56674f39
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-24 20:30:19 +0300

    Add files via upload

---
+++ 3. Калькулятор.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    std::string buffer;
+    std::cout << "Введите выражение в формате '<число-1><действие><число-2>': ";
+    std::getline(std::cin, buffer);
+
+    std::stringstream buffer_stream(buffer); //оборот строки в строковый поток для разбора
+
+    //разбор строки на три компонента
+    double num1, num2;
+    char op;
+
+    buffer_stream >> num1 >> op >> num2;
+
+    if (op == '+') {
+        std::cout << "Результат: " << num1 + num2 << std::endl;
+    }
+    else if (op == '-') {
+        std::cout << "Результат: " << num1 - num2 << std::endl;
+    }
+    else if (op == '*') {
+        std::cout << "Результат: " << num1 * num2 << std::endl;
+    }
+    else if (op == '/') {
+        if (num2 == 0) {
+            std::cout << "Ошибка: деление на ноль!" << std::endl;
+        }
+        else {
+            std::cout << "Результат: " << num1 / num2 << std::endl;
+        }
+    }
+    else {
+        std::cout << "Недопустимая операция!" << std::endl;
+    }
+}
